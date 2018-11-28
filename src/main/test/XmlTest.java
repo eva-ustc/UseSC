64a88cd8828095f2056173b12050fbad15b17e8f
eva-ustc <1326510824@qq.com>
2018-11-29 00:05:24 +0800

    E5 Completed! 添加切换数据库mysql&postgresql(Docker)

--- src/main/test/XmlTest.java
+++ src/main/test/XmlTest.java
@@ -3,7 +3,7 @@ import utils.DBCPUtils;
 import utils.XmlUtils;
 
 import java.io.File;
-import java.sql.Connection;
+import java.sql.*;
 import java.util.List;
 
 /**
@@ -23,5 +23,43 @@ public class XmlTest {
     public void testDBCP(){
         Connection connection = DBCPUtils.getConnection();
         System.out.println(connection);
+    }
+    @Test
+    public void testpostgres(){
+        Connection connection=null;
+        PreparedStatement statement =null;
+        try{
+            String url="jdbc:postgresql://192.168.56.101:5432/db_sc";
+            String user="postgres";
+            String password = "123456";
+            Class.forName("org.postgresql.Driver");
+            connection= DriverManager.getConnection(url, user, password);
+            System.out.println("是否成功连接pg数据库"+connection);
+            String sql="select * from tb_user";
+            statement=connection.prepareStatement(sql);
+            ResultSet resultSet=statement.executeQuery();
+            while(resultSet.next()){
+                String name=resultSet.getString(1);
+                System.out.println(name);
+            }
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }finally{
+            try{
+                statement.close();
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }finally{
+                try{
+                    connection.close();
+                }
+                catch(SQLException e){
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
